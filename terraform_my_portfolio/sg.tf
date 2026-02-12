252d3ad8f1826477d553f5361e41219a1e77c31b
Daniel Tavares <danielhelpdesk@gmail.com>
2026-02-12 20:21:04 -0300

    feat: adding Iac files

---
+++ terraform_my_portfolio/sg.tf
@@ -0,0 +1,39 @@
+resource "aws_security_group" "website_sg" {
+  name   = "website-sg"
+  vpc_id = "vpc-05d23acb76ff16c72"
+
+  tags = {
+    Name        = "website-sg"
+    Provisioned = "Terraform"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.website_sg.id
+  cidr_ipv4         = "179.191.31.168/32"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.website_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.website_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.website_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = -1
+}
